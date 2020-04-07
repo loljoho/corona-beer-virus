22ad13f56f3f309d7f9c39923baace2ce6f18e0d
loljoho <loljoho@users.noreply.github.com>
2020-04-06 21:35:46 -0400

    Update Sidebar icons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,13 +5,14 @@ import {
   Nav,
 } from 'react-bootstrap';
 
-import { 
-  Home,
-  Map,
-  MapPin,
-  FileText,
-  PlusCircle,
-} from 'react-feather';
+import { ReactComponent as HomeIcon } from '../../lib/icons/tachometer-alt.svg';
+import { ReactComponent as StateIcon } from '../../lib/icons/map-marker-alt.svg';
+import { ReactComponent as CountryIcon } from '../../lib/icons/flag-usa.svg';
+import { ReactComponent as AreaChartIcon } from '../../lib/icons/chart-area.svg';
+import { ReactComponent as LineChartIcon } from '../../lib/icons/chart-line.svg';
+import { ReactComponent as PieChartIcon } from '../../lib/icons/chart-pie-alt.svg';
+import { ReactComponent as MedFileIcon } from '../../lib/icons/file-medical.svg';
+import { ReactComponent as AddIcon } from '../../lib/icons/plus-circle.svg';
 
 class Sidebar extends Component {
   render() {
@@ -21,54 +22,54 @@ class Sidebar extends Component {
           <Nav className="flex-column mb-2">
             <Nav.Item>
               <Nav.Link className="active" href="/">
-                <Home className="feather" size={16} />
+                <HomeIcon className="icon" />
                 Dashboard
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link href="/states">
-                <MapPin className="feather" size={16} />
+                <StateIcon className="icon" />
                 States
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link href="/country">
-                <Map className="feather" size={16} />
+                <CountryIcon className="icon" />
                 Country
               </Nav.Link>
             </Nav.Item>
           </Nav>
 
           <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Saved reports</span>
+            <span>Special views</span>
             {/* eslint-disable-next-line */}
             <a className="d-flex align-items-center text-muted" aria-label="Add a new report">
-              <PlusCircle className="feather" />
+              <AddIcon className="icon" />
             </a>
           </h6>
           <Nav className="flex-column mb-2">
             <Nav.Item>
               <Nav.Link href="#">
-                <FileText className="feather" size={16} />
-                Current month
+                <AreaChartIcon className="icon" />
+                Integration things
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link href="#">
-                <FileText className="feather" size={16} />
-                Last quarter
+                <LineChartIcon className="icon" />
+                Pandemic stuff
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link href="#">
-                <FileText className="feather" size={16} />
-                Social engagement
+                <PieChartIcon className="icon" />
+                Pacman pies
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link href="#">
-                <FileText className="feather" size={16} />
-                Year-end sale
+                <MedFileIcon className="icon" />
+                Something medical
               </Nav.Link>
             </Nav.Item>
           </Nav>
